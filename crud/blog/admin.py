9164973d39489a9660bb127f02fad1300519c9ae
Max <qwefghnz@gmail.com>
2023-10-31 11:49:21 +0600

    registered the models

--- crud/blog/admin.py
+++ crud/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Message)
+admin.site.register(Image)
